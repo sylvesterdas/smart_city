7f816cd4d134ee2c583e1b75da979905ebc4703d
Sylvester <sylvester.das@gmail.com>
2019-03-03 14:05:54 +0530

    Merge solve

--- src/main/java/com/lenin/smart_city/controllers/AdminController.java
+++ src/main/java/com/lenin/smart_city/controllers/AdminController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 @RequestMapping(path="/admin")
 public class AdminController {
