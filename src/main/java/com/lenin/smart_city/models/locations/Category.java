3348f8b3fd8e6248705aacd96dbdf9ec775ac07a
Durgesh <durgesh.yadav@tacto.in>
2019-03-04 22:30:15 +0530

    @durgesh enhancement: category CRUD integrated

--- src/main/java/com/lenin/smart_city/models/locations/Category.java
+++ src/main/java/com/lenin/smart_city/models/locations/Category.java
@@ -1,6 +1,8 @@
 package com.lenin.smart_city.models.locations;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +11,7 @@ import javax.persistence.Table;
 public class Category {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	public long id;
 	
 	public String name;
