1470e1f282798a5d362d8244291da856e9dda03e
Sylvester Das <das.sylvester@heb.com>
2019-03-17 20:02:24 +0530

    unused imports removed

--- src/main/java/com/lenin/smart_city/repositories/AddressRepository.java
+++ src/main/java/com/lenin/smart_city/repositories/AddressRepository.java
@@ -1,10 +1,8 @@
 package com.lenin.smart_city.repositories;
 
 import com.lenin.smart_city.models.locations.Address;
-import com.lenin.smart_city.models.locations.City;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 /**
  * UserRepository
