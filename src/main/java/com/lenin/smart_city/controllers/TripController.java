1464a121033cf28433324165ac7f75a07b7557a9
Sylvester <sylvester.das@gmail.com>
2019-03-21 19:31:54 +0530

    Update TripController.java
    remove unecessary imports

--- src/main/java/com/lenin/smart_city/controllers/TripController.java
+++ src/main/java/com/lenin/smart_city/controllers/TripController.java
@@ -11,19 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -40,7 +35,6 @@ import com.lenin.smart_city.repositories.PlacesRepository;
 import com.lenin.smart_city.repositories.RoleRepository;
 import com.lenin.smart_city.repositories.TripsRepository;
 import com.lenin.smart_city.storage.StorageService;
-import com.lenin.smart_city.storage.StorageService.TYPE;
 
 @Controller
 @RequestMapping(path="/admin")
