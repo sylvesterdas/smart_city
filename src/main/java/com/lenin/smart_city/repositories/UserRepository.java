ad9ef974b2d5b1dcb8992c5e61e48571df3cd953
Sylvester Das <das.sylvester@heb.com>
2019-03-24 19:34:22 +0530

    Adds the author name automatically

--- src/main/java/com/lenin/smart_city/repositories/UserRepository.java
+++ src/main/java/com/lenin/smart_city/repositories/UserRepository.java
@@ -3,11 +3,15 @@ package com.lenin.smart_city.repositories;
 import com.lenin.smart_city.models.auth.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * UserRepository
  */
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	
+	@Query(nativeQuery=true, value="SELECT * FROM users WHERE email LIKE ? LIMIT 1")
+	User getOneByUsername(String user);
     
 }
