680c389b61081d88edaf55057321e094c21de7be
Sylvester Das <das.sylvester@heb.com>
2019-03-24 20:41:27 +0530

    migrations completed

--- src/main/java/com/lenin/smart_city/models/auth/Role.java
+++ src/main/java/com/lenin/smart_city/models/auth/Role.java
@@ -1,15 +1,18 @@
 package com.lenin.smart_city.models.auth;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
 import java.util.Set;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "roles", uniqueConstraints=@UniqueConstraint(columnNames={"name"}))
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     public Long id;
 
+    @NotBlank
     public String name;
 
     @OneToMany(mappedBy = "role")
--- src/main/java/com/lenin/smart_city/models/locations/Category.java
+++ src/main/java/com/lenin/smart_city/models/locations/Category.java
@@ -5,14 +5,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
 
 @Entity(name="categories")
-@Table(name="categories")
+@Table(name="categories", uniqueConstraints=@UniqueConstraint(columnNames={"name"}))
 public class Category {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.TABLE)
 	public long id;
 	
 	@NotBlank
